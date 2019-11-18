e4a5ef503404dde3de03b0b4b5ca78a7b7bebf60
Andreas Müller <schnitzeltony@gmail.com>
2019-11-18 19:20:59 +0100

    main.cpp: setup presistent option handling
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- src/main.cpp
+++ src/main.cpp
@@ -19,7 +19,7 @@ int main(int argc, char *argv[])
     QtWebEngine::initialize();
     QQmlApplicationEngine engine;
 
-    // register CMarkDownQt as singleton
+    CMarkDownQt::setSettingsParameters(app.organizationName(), app.applicationName());
     CMarkDownQt::registerQML();
     QtHelper::registerQML();
 
