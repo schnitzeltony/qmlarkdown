ca0867f46d7bfabe39ba26a227bcddbd2f9b8a2b
Andreas Müller <schnitzeltony@gmail.com>
2019-11-28 00:24:29 +0100

    there is no schnitzeltony.org (yet)
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- src/main.cpp
+++ src/main.cpp
@@ -14,7 +14,7 @@ int main(int argc, char *argv[])
 
     QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
     QApplication app(argc, argv);
-    app.setOrganizationName("schnitzeltony.org");
+    app.setOrganizationName("schnitzeltony");
 
     QtWebEngine::initialize();
     QQmlApplicationEngine engine;
